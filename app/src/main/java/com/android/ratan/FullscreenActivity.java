0aaa6c211e3c726d0516e4b039b911148c9b0aca
Amit Chipkar <chipkaramit@gmail.com>
2020-12-10 14:29:22 +0530

    Handled Network Connectivity.

--- app/src/main/java/com/android/ratan/FullscreenActivity.java
+++ app/src/main/java/com/android/ratan/FullscreenActivity.java
@@ -1,20 +1,10 @@
 package com.android.ratan;
 
-import android.annotation.SuppressLint;
-
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 
--- app/src/main/java/com/android/ratan/MainActivity.java
+++ app/src/main/java/com/android/ratan/MainActivity.java
@@ -1,13 +1,10 @@
 package com.android.ratan;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
@@ -19,6 +16,9 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,18 +36,26 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(MainActivity.this);
 
         webView = findViewById(R.id.web_view);
-        strURL = "http://soumitranath.in/projects/ratan/";
+        strURL = "http://ratan.club/game/";
 
-            progressDialog.setMessage(getResources().getString(R.string.please_wait));
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-            webView.setWebViewClient(new myWebViewClient());
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.getSettings().setLoadsImagesAutomatically(true);
-            webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-            webView.loadUrl(strURL);
+        progressDialog.setMessage(getResources().getString(R.string.please_wait));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        webView.setWebViewClient(new myWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(strURL);
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack())
+            webView.goBack();// if there is previous page open it
+        else
+            super.onBackPressed();//if there is no previous page, close app
     }
 
     private class myWebViewClient extends WebViewClient {
@@ -60,39 +68,35 @@ public class MainActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             // TODO Auto-generated method stub
 //            view.loadUrl(url);
-            if( URLUtil.isNetworkUrl(url) )
-            {
+            if (URLUtil.isNetworkUrl(url)) {
                 return false;
             }
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
-            }catch(ActivityNotFoundException e)
-            {
-                Log.e("Amit",e.toString());
-                Toast.makeText(MainActivity.this,"No app found to execute task",Toast.LENGTH_LONG).show();
+            } catch (ActivityNotFoundException e) {
+                Log.e("Amit", e.toString());
+                Toast.makeText(MainActivity.this, "No app found to execute task", Toast.LENGTH_LONG).show();
             }
             return true;
 
         }
+
         @RequiresApi(Build.VERSION_CODES.N)
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
-        {
-            String url=request.getUrl().toString();
-            if( URLUtil.isNetworkUrl(url) )
-            {
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
+            if (URLUtil.isNetworkUrl(url)) {
                 return false;
             }
             try {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
-            }catch(ActivityNotFoundException e)
-            {
-                Log.e("Amit",e.toString());
-                Toast.makeText(MainActivity.this,"No app found to execute task",Toast.LENGTH_LONG).show();
+            } catch (ActivityNotFoundException e) {
+                Log.e("Amit", e.toString());
+                Toast.makeText(MainActivity.this, "No app found to execute task", Toast.LENGTH_LONG).show();
             }
             return true;
         }
@@ -102,13 +106,37 @@ public class MainActivity extends AppCompatActivity {
             super.onPageFinished(view, url);
             progressDialog.dismiss();
         }
-    }
 
-    @Override
-    public void onBackPressed() {
-        if(webView!= null && webView.canGoBack())
-            webView.goBack();// if there is previous page open it
-        else
-            super.onBackPressed();//if there is no previous page, close app
+        public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+            try {
+                webView.stopLoading();
+            } catch (Exception e) {
+            }
+
+            if (webView.canGoBack()) {
+                webView.goBack();
+            }
+
+            webView.loadUrl("about:blank");
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage("Check your internet connection and try again.");
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+
+            alertDialog.show();
+            super.onReceivedError(webView, errorCode, description, failingUrl);
+        }
+
+
     }
 }
